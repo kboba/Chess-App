723323bdb55775b203e475e022754fabd28888c0
BobaK <boba.krzysztof98@gmail.com>
2021-09-14 15:50:41 +0200

    Override JFrame paint() method

--- Chess_App/src/main/java/GUI/BoardDisplay.java
+++ Chess_App/src/main/java/GUI/BoardDisplay.java
@@ -16,8 +16,28 @@ public class BoardDisplay {
     }
 
     public void initialize(){
-        frame = new JFrame();
-        frame.setBounds(0, 0, 512, 512);
+        frame = new JFrame(){
+            @Override
+            public void paint(Graphics g) {
+                boolean isWhite;
+                for(int x = 0; x < 8; x++){
+                    for(int y = 0; y < 8; y++){
+                        isWhite = (x+y)%2 == 0;
+
+                        if(isWhite){
+                            g.setColor(Color.WHITE); // color white
+                        }
+                        else{
+                            g.setColor(Color.DARK_GRAY); // black
+                        }
+
+                        g.fillRect(x*BoardDisplay.WIDTH, y*BoardDisplay.HEIGHT, BoardDisplay.WIDTH, BoardDisplay.HEIGHT);
+                    }
+                }
+            }
+        };
+
+        frame.setBounds(100, 100, 512, 512);
         panel = new JPanel();
         frame.add(panel);
         frame.setVisible(true);
