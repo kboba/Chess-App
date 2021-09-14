13c25b0797abe299bd5a4285d0ecb6c97c77413e
BobaK <boba.krzysztof98@gmail.com>
2021-09-14 22:01:00 +0200

    rebuild Board and Square class

--- Chess_App/src/main/java/Board/Board.java
+++ Chess_App/src/main/java/Board/Board.java
@@ -1,13 +1,28 @@
 package Board;
 
+import GUI.BoardDisplay;
+import Game.Position;
+import Pieces.Piece;
+
 public class Board {
     private Square[][] boardSquares;
+    private BoardDisplay boardDisplay;
 
-    public Board(){
+    public Board(BoardDisplay boardDisplay) {
+        this.boardDisplay = boardDisplay;
+        boardDisplay.initialize(boardSquares);
         initialize();
     }
 
     private void initialize() {
+//        boardSquares[0][0];
+//        boardSquares[0][1];
+//        boardSquares[0][2];
+//        boardSquares[0][3];
+//        boardSquares[0][4];
+//        boardSquares[0][5];
+//        boardSquares[0][6];
+//        boardSquares[0][7];
 
     }
 }
