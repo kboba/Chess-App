1eec64b77834f1f74e1ef8d17bba500039fbde8e
BobaK <boba.krzysztof98@gmail.com>
2021-09-20 02:04:12 +0200

    Add pawns to virtual board

--- Chess_App/src/main/java/Board/Board.java
+++ Chess_App/src/main/java/Board/Board.java
@@ -1,5 +1,8 @@
 package Board;
 
+import Pieces.Pawn;
+import Pieces.PlayerColor;
+
 public class Board {
     private Square[][] boardSquares;
 
@@ -8,15 +11,23 @@ public class Board {
     }
 
     private void initialize() {
-//        boardSquares[0][0];
-//        boardSquares[0][1];
-//        boardSquares[0][2];
-//        boardSquares[0][3];
-//        boardSquares[0][4];
-//        boardSquares[0][5];
-//        boardSquares[0][6];
-//        boardSquares[0][7];
+        boardSquares[0][1].setPiece(new Pawn(PlayerColor.WHITE, new Position(0, 1)));
+        boardSquares[1][1].setPiece(new Pawn(PlayerColor.WHITE, new Position(1, 1)));
+        boardSquares[2][1].setPiece(new Pawn(PlayerColor.WHITE, new Position(2, 1)));
+        boardSquares[3][1].setPiece(new Pawn(PlayerColor.WHITE, new Position(3, 1)));
+        boardSquares[4][1].setPiece(new Pawn(PlayerColor.WHITE, new Position(4, 1)));
+        boardSquares[5][1].setPiece(new Pawn(PlayerColor.WHITE, new Position(5, 1)));
+        boardSquares[6][1].setPiece(new Pawn(PlayerColor.WHITE, new Position(6, 1)));
+        boardSquares[7][1].setPiece(new Pawn(PlayerColor.WHITE, new Position(7, 1)));
 
+        boardSquares[0][6].setPiece(new Pawn(PlayerColor.BLACK, new Position(0, 6)));
+        boardSquares[1][6].setPiece(new Pawn(PlayerColor.BLACK, new Position(1, 6)));
+        boardSquares[2][6].setPiece(new Pawn(PlayerColor.BLACK, new Position(2, 6)));
+        boardSquares[3][6].setPiece(new Pawn(PlayerColor.BLACK, new Position(3, 6)));
+        boardSquares[4][6].setPiece(new Pawn(PlayerColor.BLACK, new Position(4, 6)));
+        boardSquares[5][6].setPiece(new Pawn(PlayerColor.BLACK, new Position(5, 6)));
+        boardSquares[6][6].setPiece(new Pawn(PlayerColor.BLACK, new Position(6, 6)));
+        boardSquares[7][6].setPiece(new Pawn(PlayerColor.BLACK, new Position(7, 6)));
     }
 
     public Square[][] getBoardSquares() {
