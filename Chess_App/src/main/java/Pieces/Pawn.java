acfe962b7c77f834253e18244876048464b7b9d9
BobaK <boba.krzysztof98@gmail.com>
2021-09-21 02:37:12 +0200

    Move move method implementation to Piece class

--- Chess_App/src/main/java/Pieces/Pawn.java
+++ Chess_App/src/main/java/Pieces/Pawn.java
@@ -14,21 +14,4 @@ public class Pawn extends Piece {
 
         return false;
     }
-
-    /*
-     * move method takes Position and Board
-     * 0. get board representation (Square[][])
-     * 1. unocupy old position
-     * 2. set new position
-     * 3. set piece on new position on board square
-     * 4. update board representation
-     */
-    @Override
-    public void move(Position newPosition, Board board) {
-        var boardSquares = board.getBoardSquares();
-        boardSquares[getPosition().getX()][getPosition().getY()].unoccupy();
-        setPosition(newPosition);
-        boardSquares[newPosition.getX()][newPosition.getY()].setPiece(this);
-        board.setBoardSquares(boardSquares);
-    }
 }
