86c70e7a597e51d517f0d11012910968794ea375
BobaK <boba.krzysztof98@gmail.com>
2021-09-29 17:28:54 +0200

    Add castle variable for King

--- Chess_App/src/main/java/Pieces/King.java
+++ Chess_App/src/main/java/Pieces/King.java
@@ -4,9 +4,19 @@ import Board.Board;
 import Board.Position;
 
 public class King extends Piece {
+    private boolean castleDone;
 
     public King(PlayerColor playerColor, Position position) {
         super(playerColor, PieceType.KING, position);
+        castleDone = false;
+    }
+
+    public boolean isCastleDone() {
+        return castleDone;
+    }
+
+    public void setCastleDone() {
+        this.castleDone = true;
     }
 
     @Override
