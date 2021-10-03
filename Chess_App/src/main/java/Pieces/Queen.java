ce5672051265769618aa221e5a4c84b02603a708
BobaK <boba.krzysztof98@gmail.com>
2021-10-03 23:06:58 +0200

    Queen move vertical

--- Chess_App/src/main/java/Pieces/Queen.java
+++ Chess_App/src/main/java/Pieces/Queen.java
@@ -2,6 +2,7 @@ package Pieces;
 
 import Board.Board;
 import Board.Position;
+import Board.Square;
 
 public class Queen extends Piece {
 
@@ -11,6 +12,28 @@ public class Queen extends Piece {
 
     @Override
     public boolean isMoveValid(Position newPosition, Board board) {
+        var currentPositionX = getPosition().getX();
+        var currentPositionY = getPosition().getY();
+        var newPositionX = newPosition.getX();
+        var newPositionY = newPosition.getY();
+        var boardSquares = board.getBoardSquares();
+
+        if(currentPositionX == newPositionX) {
+            if(currentPositionY < newPositionY)
+                for(int yPosition = currentPositionY+1; yPosition < newPositionY; yPosition++){
+                    if(boardSquares[currentPositionX][yPosition].getPiece()!=null){
+                        return false;
+                    }
+                }
+            else if(currentPositionY > newPositionY)
+                for(int yPosition = currentPositionY-1; yPosition > newPositionY; yPosition--){
+                    if(boardSquares[currentPositionX][yPosition].getPiece()!=null){
+                        return false;
+                    }
+                }
+
+            return true;
+        }
 
         return false;
     }
