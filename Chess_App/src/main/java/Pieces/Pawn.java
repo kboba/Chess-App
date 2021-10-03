37f65d1ce3830ce9062917bad14fcd09053c7d2e
BobaK <boba.krzysztof98@gmail.com>
2021-10-03 17:51:30 +0200

    Implement Pawn can move forward

--- Chess_App/src/main/java/Pieces/Pawn.java
+++ Chess_App/src/main/java/Pieces/Pawn.java
@@ -4,6 +4,7 @@ import Board.Board;
 import Board.Position;
 
 public class Pawn extends Piece {
+    private boolean firstMoveDone;
 
     public Pawn(PlayerColor playerColor, Position position) {
         super(playerColor, PieceType.PAWN, position);
@@ -11,6 +12,30 @@ public class Pawn extends Piece {
 
     @Override
     public boolean isMoveValid(Position newPosition, Board board) {
+        var currentPositionX = getPosition().getX();
+        var currentPositionY = getPosition().getY();
+        var newPositionX = newPosition.getX();
+        var newPositionY = newPosition.getY();
+
+        if(getPlayerColor()==PlayerColor.WHITE){
+            if(newPositionY==currentPositionY+1) {
+                firstMoveDone = true;
+                return true;
+            }
+            if(newPositionY==currentPositionY+2 && !firstMoveDone) {
+                firstMoveDone = true;
+                return true;
+            }
+        } else {
+            if(newPositionY==currentPositionY-1) {
+                firstMoveDone = true;
+                return true;
+            }
+            if(newPositionY==currentPositionY-2 && !firstMoveDone) {
+                firstMoveDone = true;
+                return true;
+            }
+        }
 
         return false;
     }
