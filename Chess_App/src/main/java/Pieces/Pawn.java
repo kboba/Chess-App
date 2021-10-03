b187fcc6ad07ed4399f9a2268359fc91480f514d
BobaK <boba.krzysztof98@gmail.com>
2021-10-03 22:45:22 +0200

    Refactor of Pawn class

--- Chess_App/src/main/java/Pieces/Pawn.java
+++ Chess_App/src/main/java/Pieces/Pawn.java
@@ -19,21 +19,20 @@ public class Pawn extends Piece {
         var currentPositionY = getPosition().getY();
         var newPositionX = newPosition.getX();
         var newPositionY = newPosition.getY();
-        Square[][] boardSquares = board.getBoardSquares();
+        var boardSquares = board.getBoardSquares();
 
         if (currentPositionX == newPositionX)
             return canPawnMoveForward(currentPositionY, boardSquares[newPositionX], newPositionY);
 
         if (abs(currentPositionX-newPositionX)==1) {
-            return canPawnTakeDiagonally(currentPositionY, boardSquares[newPositionX][newPositionY], newPositionY);
+            return canPawnTakeDiagonally(currentPositionY, boardSquares[newPositionX][newPositionY].getPiece(), newPositionY);
         }
 
         return false;
     }
 
 
-    private boolean canPawnTakeDiagonally(int currentPositionY, Square newPositionSquare, int newPositionY) {
-        Piece pieceOnNewPositionSquare = newPositionSquare.getPiece();
+    private boolean canPawnTakeDiagonally(int currentPositionY, Piece pieceOnNewPositionSquare, int newPositionY) {
         if (pieceOnNewPositionSquare != null){
             if (getPlayerColor() == PlayerColor.WHITE) {
                 if (newPositionY == currentPositionY + 1) {
