0df5e48d249a6140b0f705abedd3451e1265fd3f
BobaK <boba.krzysztof98@gmail.com>
2021-10-03 23:13:57 +0200

    Rocks can move horizontally and vertically

--- Chess_App/src/main/java/Pieces/Rock.java
+++ Chess_App/src/main/java/Pieces/Rock.java
@@ -2,6 +2,7 @@ package Pieces;
 
 import Board.Board;
 import Board.Position;
+import Board.Square;
 
 public class Rock extends Piece {
 
@@ -11,7 +12,50 @@ public class Rock extends Piece {
 
     @Override
     public boolean isMoveValid(Position newPosition, Board board) {
+        var currentPositionX = getPosition().getX();
+        var currentPositionY = getPosition().getY();
+        var newPositionX = newPosition.getX();
+        var newPositionY = newPosition.getY();
+        var boardSquares = board.getBoardSquares();
+
+        if(currentPositionX == newPositionX)
+            return canMoveVertically(currentPositionX, currentPositionY, newPositionY, boardSquares);
+
+        if(currentPositionY == newPositionY)
+            return canMoveHorizontally(currentPositionX, currentPositionY, newPositionX, boardSquares);
 
         return false;
+    }
+
+    private boolean canMoveHorizontally(int currentPositionX, int currentPositionY, int newPositionX, Square[][] boardSquares) {
+        if(currentPositionX < newPositionX)
+            for(int xPosition = currentPositionX +1; xPosition < newPositionX; xPosition++){
+                if(boardSquares[xPosition][currentPositionY].getPiece()!=null)
+                    return false;
+            }
+        else if(currentPositionX > newPositionX)
+            for(int xPosition = currentPositionX -1; xPosition > newPositionX; xPosition--){
+                if(boardSquares[xPosition][currentPositionY].getPiece()!=null)
+                    return false;
+            }
+
+        return true;
+    }
+
+    private boolean canMoveVertically(int currentPositionX, int currentPositionY, int newPositionY, Square[][] boardSquares) {
+        if(currentPositionY < newPositionY)
+            for(int yPosition = currentPositionY +1; yPosition < newPositionY; yPosition++){
+                if(boardSquares[currentPositionX][yPosition].getPiece()!=null){
+                    return false;
+                }
+            }
+        else if(currentPositionY > newPositionY)
+            for(int yPosition = currentPositionY -1; yPosition > newPositionY; yPosition--){
+                if(boardSquares[currentPositionX][yPosition].getPiece()!=null){
+                    return false;
+                }
+            }
+
+        return true;
     }
 }
