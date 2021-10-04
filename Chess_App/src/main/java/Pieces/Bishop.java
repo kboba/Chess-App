77f4f1f5232d5afac5bad06bb92e0f64793e3d0c
BobaK <boba.krzysztof98@gmail.com>
2021-10-04 18:21:09 +0200

    Bishop can move diagonally

--- Chess_App/src/main/java/Pieces/Bishop.java
+++ Chess_App/src/main/java/Pieces/Bishop.java
@@ -2,6 +2,9 @@ package Pieces;
 
 import Board.Board;
 import Board.Position;
+import Board.Square;
+
+import static java.lang.Math.abs;
 
 public class Bishop extends Piece {
 
@@ -11,7 +14,50 @@ public class Bishop extends Piece {
 
     @Override
     public boolean isMoveValid(Position newPosition, Board board) {
+        var currentPositionX = getPosition().getX();
+        var currentPositionY = getPosition().getY();
+        var newPositionX = newPosition.getX();
+        var newPositionY = newPosition.getY();
+        var boardSquares = board.getBoardSquares();
+
+
+        if(abs(currentPositionX-newPositionX)==abs(currentPositionY-newPositionY)) {
+            int xPosition = currentPositionX;
+            int yPosition = currentPositionY;
+            byte xDirection = 0;
+            byte yDirection = 0;
+            if(currentPositionX < newPositionX && currentPositionY < newPositionY) {
+                xDirection = 1;
+                yDirection = 1;
+            }
+            else if(currentPositionX < newPositionX && currentPositionY > newPositionY){
+                xDirection = 1;
+                yDirection =-1;
+            }
+            else if(currentPositionX > newPositionX && currentPositionY > newPositionY){
+                xDirection =-1;
+                yDirection =-1;
+            }
+            else if(currentPositionX > newPositionX && currentPositionY < newPositionY){
+                xDirection =-1;
+                yDirection = 1;
+            }
+
+            xPosition+=xDirection;
+            yPosition+=yDirection;
+
+            while(xPosition != newPositionX && yPosition != newPositionY){
+                if(boardSquares[xPosition][yPosition].getPiece()!=null){
+                    return false;
+                }
+                xPosition+=xDirection;
+                yPosition+=yDirection;
+            }
+
+            return true;
+        }
 
         return false;
     }
+
 }
